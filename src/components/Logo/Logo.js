98e0ad31452d6f21ddd50ed9a430a2f5babf8208
Gonçalo Regalado <gf.regalado@gmail.com>
2020-05-31 14:39:37 +0100

    changes most styles in mobile view and some in desktop view

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,10 +3,6 @@ import React from 'react';
 import burgerLogo from '../../assets/Logo/original.png';
 import classes from './Logo.module.css';
 
-const logo = (props) => (
-  <div className={classes.Logo}>
-    <img src={burgerLogo} alt="burguer-logo" />
-  </div>
-);
+const logo = (props) => <h1>Saucy Burger</h1>;
 
 export default logo;
